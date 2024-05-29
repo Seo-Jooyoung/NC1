5576ac388ee73c5eff856699fea72dd3ffea7091
Seo Jooyoung <jooyoungseo02@gmail.com>
2024-05-29 20:38:22 +0900

    Initial commit

---
+++ NC1/NC1/NC1App.swift
@@ -0,0 +1,17 @@
+//
+//  NC1App.swift
+//  NC1
+//
+//  Created by Seo-Jooyoung on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct NC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
